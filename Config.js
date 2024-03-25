5db2deb71f1667585e3473e11c3eec9a8f3f7163
arielaizn <152599006+arielaizn@users.noreply.github.com>
2024-03-25 17:16:49 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['972504586616']
+global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//פרופיבוט
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //פרופיבוט
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
